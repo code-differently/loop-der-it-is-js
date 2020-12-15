9aa74e603e0417e1f307ed25fe908ba675a243bb
Kalyn Smith <kalyn@Kalyns-MacBook-Air.local>
2020-12-14 20:01:36 -0500

    progress

--- Loops.js
+++ Loops.js
@@ -1,8 +1,42 @@
 class LoopDeLoop {
-
-
-
+        oneToTen(){
+            let response ="";
+            for (let i=1; i <= 10; i++){
+              response += i+"\n";
+            }
+            return response;
+        }
+        loopsSquared(){
+          let answer ="";
+          for (let i =1; i <= 10; i++){
+            answer += (i * i) + "\n";
+          }
+          return answer;
+        }
+        evenUnder(n){
+          let output = "";
+          for (let i = 2; i < n; i+=2){
+            output += i + "\n";
+          }
+          return output;
+    
+        }
+        numberSum(n,m){
+            let total = 0;
+            for (let i = n; i < m; i++){
+                total += i;
+            }
+            return total;
+        }
+        triangle(){
+            let output = " ";
+            for( let i = 1; i < 6; i++ ){
+                output += i = " ";
+            }
+            return output;
+        }
 }
 
-
-module.exports = LoopDeLoop;
+    
+    module.exports = LoopDeLoop;
+    
